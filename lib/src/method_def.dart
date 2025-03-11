e91f0893d338aae505d2d8b5a78ef30a2873f552
dabblingwithcode <nacho.dominguis@proton.me>
2025-03-11 10:01:50 +0100

    ...

--- lib/src/method_def.dart
+++ lib/src/method_def.dart
@@ -36,11 +36,11 @@ class MethodDef {
     final buffer = StringBuffer();
     if (declaration.parameters != null) {
       for (final parameter in declaration.parameters!.parameters) {
-        final parameterType = parameter is SimpleFormalParameter
-            ? parameter.type?.toString()
-            : 'unknown';
+        final parameterType = parameter.declaredElement?.type
+                .getDisplayString(withNullability: false) ??
+            'unknown';
         buffer.write(
-            '${parameterType?.wrapWithColor(FontColor.type)} ${parameter.name?.toString().wrapWithColor(FontColor.functionArgument) ?? 'no name found'}, ');
+            '${parameterType.wrapWithColor(FontColor.type)} ${parameter.name?.toString().wrapWithColor(FontColor.functionArgument) ?? 'no name found'}, ');
       }
       parameters = buffer.toString();
     } else {
